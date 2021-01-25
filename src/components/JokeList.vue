7ecd37b42216318601f043720cf3192f462e0c7d
YovelBecker <yovelb13@gmail.com>
2021-01-26 01:22:53 +0200

    added joke list component

---
+++ src/components/JokeList.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-if="jokes">
+    <JokePreview v-for="joke in jokes" :joke="joke" :key="joke.id" />
+  </template>
+</template>
+
+<script>
+import jokeService from '../services/jokeService';
+import JokePreview from './JokePreview';
+
+export default {
+  data() {
+    return {
+      jokes: null
+    }
+  },
+  created() {
+    this.jokes = jokeService.getJokes();
+  },
+  components: {
+    JokePreview
+  }
+}
+</script>
