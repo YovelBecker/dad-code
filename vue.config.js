78e785771773068fb9273b54a0371a61a99a8963
YovelBecker <yovelb13@gmail.com>
2021-01-26 01:52:30 +0200

    added pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dad-code/'
-    : '/'
+    : '/',
+  pwa: {
+    name: `Daddy-O'`,
+    themeColor: "#892cdc",
+    msTileColor: "#892cdc",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: null,
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  },
 }
