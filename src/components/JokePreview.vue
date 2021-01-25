4bfec796c40cfa656d07e575f8f5599e8e81a248
YovelBecker <yovelb13@gmail.com>
2021-01-26 01:23:10 +0200

    added joke preview component

---
+++ src/components/JokePreview.vue
@@ -0,0 +1,15 @@
+<template>
+  <section v-if="joke" class="joke-preview-container">
+    <!-- <h1 class="title">You have been given a gift:</h1> -->
+    <h5>{{ joke.setup }}</h5>
+    <h4>{{ joke.punchline }}</h4>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    joke: { type: Object, required: true }
+  }
+}
+</script>
