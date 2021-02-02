17d0cfb3c79ba55e76d1153fcdd3db6f64fc0721
YovelBecker <yovelb13@gmail.com>
2021-02-03 01:07:51 +0200

    added beforeinstall event

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>Welcome To Daddy</h1>
   </header>
+  <div class="btn btn-outline-success sticky-top" @click="installer" :style="{ display: installBtn }">Install</div>
   <main>
     <section class="nav-tabs-container">
       <router-link :to="{ name: 'curr-joke' }">Today's Joke</router-link>
@@ -17,5 +18,32 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      installBtn: 'none',
+      installer: undefined
+    }
+  },
+  created() {
+    let installPrompt;
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      installPrompt = e;
+      this.installBtn = 'block';
+    })
+
+    this.installer = () => {
+      this.installBtn = 'none';
+      installPrompt.prompt();
+      installPrompt.userChoice.then(result => {
+        if (result.outcome === 'accepted') {
+          console.log('user accepted');
+        } else {
+          console.log('user denied');
+        }
+        installPrompt = null;
+      })
+    }
+  }
 }
 </script>
