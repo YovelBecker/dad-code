2516ecdba1458b58534b2582f27f2ea78641dbdf
YovelBecker <yovelb13@gmail.com>
2021-02-02 23:12:13 +0200

    testing public path value

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dad-code/'
+    ? 'https://yovelbecker.github.io/dad-code/'
     : '/',
   pwa: {
     name: `Daddy-O'`,
