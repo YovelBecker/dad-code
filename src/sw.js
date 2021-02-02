9efb3a2f17bf0930bc92b809793d4f86bf8c41d7
YovelBecker <yovelb13@gmail.com>
2021-02-03 00:41:33 +0200

    commented supress warnings function

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 console.log('service worker Registered!')
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.supressWarnings();
+// workbox.precaching.supressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener('message', msg => {
