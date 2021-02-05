6c38327ad9a904e415dcb45be6fee2589a1545e9
YovelBecker <yovelb13@gmail.com>
2021-02-05 23:23:17 +0200

    testing service worker code

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,3 +14,14 @@ firebase.initializeApp(firebaseConfig)
 
 const messaging = firebase.messaging();
 
+console.log('service worker Registered!')
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.supressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener('message', msg => {
+  if (msg.data.action == 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
