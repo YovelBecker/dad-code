8e38d1e21aaadcaa8982166e78f10d6eb9b29025
YovelBecker <yovelb13@gmail.com>
2021-02-06 01:53:59 +0200

    added firebase sw

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js');
+
 console.log('service worker Registered!')
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
@@ -8,3 +11,15 @@ self.addEventListener('message', msg => {
     self.skipWaiting();
   }
 });
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB2jgltktu4rIlxs6de7ClxtEKOnmL9hA8",
+  authDomain: "dad-code.firebaseapp.com",
+  projectId: "dad-code",
+  storageBucket: "dad-code.appspot.com",
+  messagingSenderId: "230175800309",
+  appId: "1:230175800309:web:a8ad53cb5c3285ce4fbebc"
+};
+firebase.initializeApp(firebaseConfig)
+
+const messaging = firebase.messaging();
